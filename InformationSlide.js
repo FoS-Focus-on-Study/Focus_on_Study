515807e14df04056330b7a1e0a408366a84cdbf7
wijihye <58647487+wijihye@users.noreply.github.com>
2020-11-25 03:15:40 +0900

    20.11.25_modify_git_merge
    git에서 merge하는 과정에서 충돌난 코드 및 누락된 부분을 수정

--- InformationSlide.js
+++ InformationSlide.js
@@ -1,39 +1,51 @@
-const slideButtonPrev = document.querySelector(".siteInformationBox .slideBox button.prev");
-    slideButtonNext = document.querySelector(".siteInformationBox .slideBox button.next");
-    firstSlide = document.querySelector(".siteInformationBox .slideBox .slideImages img:first-child");
-    lastSlide = document.querySelector(".siteInformationBox .slideBox .slideImages img:last-child");
+const slideButtonPrev = document.querySelector(
+  ".siteInformationBox .slideBox button.prev"
+);
+slideButtonNext = document.querySelector(
+  ".siteInformationBox .slideBox button.next"
+);
+firstSlide = document.querySelector(
+  ".siteInformationBox .slideBox .slideImages img:first-child"
+);
+lastSlide = document.querySelector(
+  ".siteInformationBox .slideBox .slideImages img:last-child"
+);
 
 const SHOWING_CLASS = "showing";
 
-function movePrevEvent(){
-    // slideBox.style.backgroundImage = "url('./images/slideImage3.jpg')"
-    const currentSlide = document.querySelector(".siteInformationBox .slideImages img.showing");
+function movePrevEvent() {
+  // slideBox.style.backgroundImage = "url('./images/slideImage3.jpg')"
+  const currentSlide = document.querySelector(
+    ".siteInformationBox .slideImages img.showing"
+  );
 
-    if(currentSlide){
-        currentSlide.classList.remove(SHOWING_CLASS);
-        const prevSlide = currentSlide.previousElementSibling;
-        if(prevSlide){
-            prevSlide.classList.add(SHOWING_CLASS);
-        } else {
-            lastSlide.classList.add(SHOWING_CLASS);
-        }
+  if (currentSlide) {
+    currentSlide.classList.remove(SHOWING_CLASS);
+    const prevSlide = currentSlide.previousElementSibling;
+    if (prevSlide) {
+      prevSlide.classList.add(SHOWING_CLASS);
+    } else {
+      lastSlide.classList.add(SHOWING_CLASS);
     }
+  }
 }
 
-function moveNextEvent(){
-    // slideBox.style.backgroundImage = "url('./images/slideImage2.jpg')"
-    const currentSlide = document.querySelector(".siteInformationBox .slideImages img.showing");
-    if(currentSlide){
-        currentSlide.classList.remove(SHOWING_CLASS);
-        const nextSlide = currentSlide.nextElementSibling;
-        if(nextSlide){
-            nextSlide.classList.add(SHOWING_CLASS);
-        } else {
-            firstSlide.classList.add(SHOWING_CLASS);
-        }
+function moveNextEvent() {
+  // slideBox.style.backgroundImage = "url('./images/slideImage2.jpg')"
+  const currentSlide = document.querySelector(
+    ".siteInformationBox .slideImages img.showing"
+  );
+  if (currentSlide) {
+    currentSlide.classList.remove(SHOWING_CLASS);
+    const nextSlide = currentSlide.nextElementSibling;
+    if (nextSlide) {
+      nextSlide.classList.add(SHOWING_CLASS);
     } else {
-        firstSlide.classList.add(SHOWING_CLASS);
+      firstSlide.classList.add(SHOWING_CLASS);
     }
+  } else {
+    firstSlide.classList.add(SHOWING_CLASS);
+  }
 }
 
 slideButtonPrev.onclick = movePrevEvent;
