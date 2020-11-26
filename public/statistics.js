f0c48fd5e59978142f0f699c65db2524e9793e83
DESKTOP-76V6K1K\se5ng <c9604100@gmail.com>
2020-11-26 18:24:30 +0900

    통계페이지 수정 & prettier 적용됨
    css : .statisticsBox > .main > .text 위치 조정
    
    js : 1. 페이지 로드시 오늘날짜의 통계데이터가 출력되는데, 데이터가 없을경우 빈화면.
          2. myChart 객체가 생성되었을 경우에만 destroy 함수 사용. (if문)

--- public/statistics.js
+++ public/statistics.js
@@ -2,14 +2,14 @@ const sampleData = {
   day20201121: {
     study: [
       {
-        subject: "프론트엔드 공부",
+        subject: '프론트엔드 공부',
         // startTime: "14:30:00",
         // endTime: "15:30:00",
         totalTime: 3600,
         // totalTime: "1:0:0",
       },
       {
-        subject: "dsc 프로젝트 구현",
+        subject: 'dsc 프로젝트 구현',
         // startTime: "16:30:00",
         // endTime: "17:45:30",
         totalTime: 4530,
@@ -20,43 +20,48 @@ const sampleData = {
   day20201125: {
     study: [
       {
-        subject: "미술",
+        subject: '미술',
         // startTime: "14:30:00",
         // endTime: "15:30:00",
         totalTime: 4000,
         // totalTime: "1:0:0",
       },
       {
-        subject: "체육",
+        subject: '체육',
         // startTime: "16:30:00",
         // endTime: "17:45:30",
         totalTime: 4530,
         // totalTime: "1:15:30",
       },
       {
-        subject: "음악",
+        subject: '음악',
         totalTime: 3500,
       },
     ],
   },
 };
 
-const inputDate = document.querySelector(".statisticsBox .inputDate");
-const statisticsMain = document.querySelector(".statisticsBox .main");
+const inputDate = document.querySelector('.statisticsBox .inputDate');
+const statisticsMain = document.querySelector('.statisticsBox .main');
 const dateSendButton = document.querySelector(
-  ".statisticsBox .sendDate .submitBtn"
+  '.statisticsBox .sendDate .submitBtn',
 );
-const canvas = document.querySelector(".statisticsBox #myChart");
-const mainText = document.querySelector(".statisticsBox .main .text");
+const canvas = document.querySelector('.statisticsBox #myChart');
+const mainText = document.querySelector('.statisticsBox .main .text');
 
 let myChart;
 
 function createChart(dailyData) {
+  //이전에 출력돼있던 그래프 제거.
   if (myChart !== undefined) {
     myChart.destroy();
   }
 
-  const ctx = canvas.getContext("2d");
+  //해당날짜 데이터 없을경우 함수 종료.
+  if (dailyData === undefined) {
+    return;
+  }
+  const ctx = canvas.getContext('2d');
   // ctx.clearRect(0, 0, canvas.width, canvas.height);
   // canvas.clear();
   const chartLabel = [],
@@ -73,22 +78,22 @@ function createChart(dailyData) {
   console.log(chartLabel, chartData);
   // ctx = null;
   myChart = new Chart(ctx, {
-    type: "bar",
+    type: 'bar',
     data: {
       // labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
       labels: chartLabel,
       datasets: [
         {
-          label: "studying time",
+          label: 'studying time',
           // data: [12, 19, 3, 5, 2, 3],
           data: chartData,
           backgroundColor: [
-            "rgba(255, 99, 132, 0.2)",
-            "rgba(54, 162, 235, 0.2)",
-            "rgba(255, 206, 86, 0.2)",
-            "rgba(75, 192, 192, 0.2)",
-            "rgba(153, 102, 255, 0.2)",
-            "rgba(255, 159, 64, 0.2)",
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
           ],
         },
       ],
@@ -112,7 +117,7 @@ function createChart(dailyData) {
           label: function (tooltipItem, data) {
             return (
               data.datasets[tooltipItem.datasetIndex].label +
-              ": " +
+              ': ' +
               epoch_to_hh_mm_ss(tooltipItem.yLabel)
             );
           },
@@ -135,21 +140,33 @@ function epoch_to_hh_mm_ss(epoch) {
 // }
 
 function sendDateEvent(e) {
-  if (inputDate.value === "") {
+  //날짜가 잘못됐을경우
+  if (inputDate.value === '') {
     e.preventDefault();
-    console.log("날짜를 다시 확인해주세요");
+
+    //이전에 출력돼있던 그래프 제거.
+    if (myChart) {
+      myChart.destroy();
+    }
+
+    mainText.innerHTML = '날짜를 다시 확인해주세요';
   } else {
     e.preventDefault();
-    const date = inputDate.value.split("-").join("");
+    const date = inputDate.value.split('-').join('');
     const dailyData = sampleData[`day${date}`];
     // console.log(inputDate.value);
 
+    //DB에 해당날짜 데이터가 없을경우
     if (dailyData === undefined) {
-      myChart.destroy();
-      mainText.innerHTML = "해당 날짜에 데이터가 없습니다.";
+      //이전에 출력돼있던 그래프 제거.
+      if (myChart) {
+        myChart.destroy();
+      }
+
+      mainText.innerHTML = '해당 날짜에 데이터가 없습니다.';
     } else {
       console.log(dailyData);
-      mainText.innerHTML = "";
+      mainText.innerHTML = '';
       createChart(dailyData);
     }
   }
