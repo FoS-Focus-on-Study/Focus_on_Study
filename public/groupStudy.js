82e954be9c6930bb1bdd8a522393eb1546f70d88
DESKTOP-76V6K1K\se5ng <c9604100@gmail.com>
2021-02-17 20:52:39 +0900

    selectGroup.js 코드를 groupStudy.js 로 합침.

--- public/groupStudy.js
+++ public/groupStudy.js
@@ -1,6 +1,17 @@
-const selectGroupForm = document.querySelector('.selectGroupForm');
+const selectForm = document.selectGroupForm;
+const selectOptions = document.querySelector('.selectGroup');
 const groupToDoBox = document.querySelector('.groupToDoBox');
 const liveListBox = document.querySelector('.liveListBox');
+
+function selectGroup() {
+  selectGroupForm.submit();
+  selectOptions.blur();
+}
+
+function selectGroupDefault() {
+  groupToDoBox.style.display = 'none';
+  liveListBox.style.display = 'none';
+}
 
 function createGroup() {
   window.open(
@@ -8,11 +19,6 @@ function createGroup() {
     'Create Group',
     'toolbar=no, width=360px, height=550px, directories=no, status=no,    scrollorbars=no, resizable=no',
   );
-}
-
-function selectGroupDefault() {
-  groupToDoBox.style.display = 'none';
-  liveListBox.style.display = 'none';
 }
 
 function init() {
